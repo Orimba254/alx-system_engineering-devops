a926e1a88fcfa2feb61dbd7cffdb84db74c7b41b
Orimba254 <clintonorimba@gmail.com>
2024-04-21 00:55:06 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,19 @@
 #!/usr/bin/pup
 # installing a specific version of flask 2.1.0
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# 1-install_a_package.pp
+class python_setup {
+  package { 'python3-pip':
+    ensure => installed,
+  }
 }
+
+class flask_install {
+  package { 'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    require  => Class['python_setup'],  # Ensure pip3 is installed first
+  }
+}
+
+include python_setup
+include flask_install
